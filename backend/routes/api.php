ca9f95321843b7f033a388e9595153d9326c4d2e
Santa Rosa Distribuidora <yuriramoscc@gmail.com>
2021-01-19 11:42:59 -0300

    configs

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -9,3 +9,16 @@ Route::get('/','CurriculumsController@index');
 Route::get('/show/{id}', 'CurriculumsController@show');
 Route::post('/create', 'CurriculumsController@create');
 Route::post('/update/{id}', 'CurriculumsController@update');
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
